83e5f0e0f822337122aa1d7d7d5fa98e8dd691c7
Umar Niazi <uhkniazi@gmail.com>
2021-01-14 14:29:56 +0000

    applied the filters for detecting and removing outliers for the induced KO vs induced WT contrasts

--- 11_vennDiagram.R
+++ 11_vennDiagram.R
@@ -24,12 +24,6 @@ sapply(ldfData, function(df) identical(rownames(df), rn))
 cvTitle = gsub('results//DEAnalysis_', '', names(ldfData))
 cvTitle = gsub('.xls', '', cvTitle)
 
-for (i in seq_along(cvTitle)){
-  df = ldfData[[i]]
-  hist(df$logFC, xlab='Log Fold Change', ylab='', main=paste('Fold Changes ', cvTitle[i], sep=''))
-  f_plotVolcano(df, cvTitle[i], 0.01, fc.lim=range(df$logFC))
-}
-
 names(ldfData)
 ## select significant genes
 dfContrast1.sub = ldfData[[1]][ldfData[[1]]$adj.P.Val < 0.01 & abs(ldfData[[1]]$logFC) > 0.5,]
@@ -43,9 +37,6 @@ dfContrast8.sub = ldfData[[8]][ldfData[[8]]$adj.P.Val < 0.01 & abs(ldfData[[8]]$
 dfContrast9.sub = ldfData[[9]][ldfData[[9]]$adj.P.Val < 0.01 & abs(ldfData[[9]]$logFC) > 0.5,]
 dfContrast10.sub = ldfData[[10]][ldfData[[10]]$adj.P.Val < 0.01 & abs(ldfData[[10]]$logFC) > 0.5,]
 
-
-library(VennDiagram)
-
 # create a list for overlaps
 lVenn = list(rownames(dfContrast1.sub), rownames(dfContrast2.sub), rownames(dfContrast3.sub),
              rownames(dfContrast4.sub), rownames(dfContrast5.sub), rownames(dfContrast6.sub),
@@ -54,72 +45,10 @@ lVenn = list(rownames(dfContrast1.sub), rownames(dfContrast2.sub), rownames(dfCo
 )
 names(ldfData)
 names(lVenn) = cvTitle
-# calculate overlaps
-#lVenn.overlap = calculate.overlap(lVenn)
-cvTitle[c(1, 3, 5)]
-venn.diagram(lVenn[c(1, 3, 5)], filename = 'results/venn_all_contrasts_DiffMutVsDiffWT.tif', margin=0.1)
-cvTitle[c(2, 4, 6, 7)]
-venn.diagram(lVenn[c(2, 4, 6, 7)], filename = 'results/venn_all_contrasts_DiffVsUndiff.tif', margin=0.1)
-cvTitle[c(8, 9, 10)]
-venn.diagram(lVenn[c(8, 9, 10)], filename = 'results/venn_all_contrasts_UndiffMutVsUndiffWT.tif', margin=0.1)
 
-## skip this section of separating them into up and down as this makes too many
-## combinations for this data set
-### repeat the analysis but separate up and down regulated genes
-## select significant genes
-# dfContrast1.up = dfContrast1.sub[dfContrast1.sub$logFC > 0, ]
-# dfContrast1.down = dfContrast1.sub[dfContrast1.sub$logFC < 0, ]
-# 
-# dfContrast2.up = dfContrast2.sub[dfContrast2.sub$logFC > 0, ]
-# dfContrast2.down = dfContrast2.sub[dfContrast2.sub$logFC < 0, ]
-# 
-# dfContrast3.up = dfContrast3.sub[dfContrast3.sub$logFC > 0, ]
-# dfContrast3.down = dfContrast3.sub[dfContrast3.sub$logFC < 0, ]
-# 
-# dfContrast4.up = dfContrast4.sub[dfContrast4.sub$logFC > 0, ]
-# dfContrast4.down = dfContrast4.sub[dfContrast4.sub$logFC < 0, ]
-# 
-# dfContrast5.up = dfContrast5.sub[dfContrast5.sub$logFC > 0, ]
-# dfContrast5.down = dfContrast5.sub[dfContrast5.sub$logFC < 0, ]
-# 
-# dfContrast6.up = dfContrast6.sub[dfContrast6.sub$logFC > 0, ]
-# dfContrast6.down = dfContrast6.sub[dfContrast6.sub$logFC < 0, ]
-# 
-# dfContrast7.up = dfContrast7.sub[dfContrast7.sub$logFC > 0, ]
-# dfContrast7.down = dfContrast7.sub[dfContrast7.sub$logFC < 0, ]
-# 
-# dfContrast8.up = dfContrast8.sub[dfContrast8.sub$logFC > 0, ]
-# dfContrast8.down = dfContrast8.sub[dfContrast8.sub$logFC < 0, ]
-# 
-# dfContrast9.up = dfContrast9.sub[dfContrast9.sub$logFC > 0, ]
-# dfContrast9.down = dfContrast9.sub[dfContrast9.sub$logFC < 0, ]
-# 
-# dfContrast10.up = dfContrast10.sub[dfContrast10.sub$logFC > 0, ]
-# dfContrast10.down = dfContrast10.sub[dfContrast10.sub$logFC < 0, ]
-# 
-# 
-# # create a list for overlaps
-# lVenn = list(rownames(dfContrast1.up), rownames(dfContrast1.down),
-#              rownames(dfContrast2.up), rownames(dfContrast2.down), 
-#              rownames(dfContrast3.up), rownames(dfContrast3.down),
-#              rownames(dfContrast4.up), rownames(dfContrast4.down),
-#              rownames(dfContrast5.up), rownames(dfContrast5.down),
-#              rownames(dfContrast6.up), rownames(dfContrast6.down),
-#              rownames(dfContrast7.up), rownames(dfContrast7.down),
-#              rownames(dfContrast8.up), rownames(dfContrast8.down),
-#              rownames(dfContrast9.up), rownames(dfContrast9.down),
-#              rownames(dfContrast10.up), rownames(dfContrast10.down)
-# )
-# 
-# 
-# #cvTitle = gsub('results//DEAnalysis(\\w+)VsControl.xls', '\\1', names(ldfData))
-# cvTitle.up = paste(cvTitle, 'up', sep='-')
-# cvTitle.down = paste(cvTitle, 'down', sep='-')
-# o = c(1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19, 10, 20)
-# ## sanity check
-# matrix(c(cvTitle.up, cvTitle.down)[o], ncol = 2, byrow = T)
-# cvTitle = c(cvTitle.up, cvTitle.down)[o]
-# names(lVenn) = cvTitle
+cvTitle[c(1, 3, 5)]
+cvTitle[c(2, 4, 6)]
+cvTitle[c(8, 9, 10)]
 
 ## create a binary matrix
 cvCommonGenes = unique(do.call(c, lVenn))
@@ -130,41 +59,11 @@ for (i in 1:ncol(mCommonGenes)){
 rownames(mCommonGenes) = cvCommonGenes
 colnames(mCommonGenes) = names(lVenn)
 
-# create groups in the data based on permutation with repetition: 2 ^ ncol 
-# https://www.mathsisfun.com/combinatorics/combinations-permutations.html
-mCommonGenes.grp = mCommonGenes
-set.seed(123)
-dm = dist(mCommonGenes.grp, method='binary')
-hc = hclust(dm)
-plot(hc)
-# cut the tree at the bottom to create groups
-cp = cutree(hc, h = 0.1)
-# sanity checks
-table(cp)
-length(cp)
-length(unique(cp))
-
-mCommonGenes.grp = cbind(mCommonGenes.grp, cp)
-
-### print and observe this table and select the groups you are interested in
-temp = mCommonGenes.grp
-temp = (temp[!duplicated(cp),])
-temp2 = cbind(temp, table(cp))
-rownames(temp2) = NULL
-print(temp2)
-
-## put these results together
-dfCommonGenes = data.frame(mCommonGenes, sig.pvals=rowSums(mCommonGenes), groups=cp, Symbol=ldfData[[1]][cvCommonGenes, 'SYMBOL'])
-## gene names have an X before them remove those if present
-rownames(dfCommonGenes) = (gsub(pattern = '^X', replacement = '', x = rownames(dfCommonGenes)))
-head(dfCommonGenes)
-
-write.csv(dfCommonGenes, file='results/commonDEGenes.xls')
-
 dfCommonGenes = read.csv('results/commonDEGenes.xls', header=T, stringsAsFactors = F)
 colnames(dfCommonGenes)[1] = 'ENTREZID'
 data.frame(colnames(dfCommonGenes))
 
+## choose the appropriate combination of contrasts
 m = as.matrix(dfCommonGenes[,c(2, 4, 6)])
 head(m)
 i = which(rowSums(m) >= 2)
@@ -173,39 +72,66 @@ length(i)
 dfCommonGenes.ind = dfCommonGenes[i,]
 dim(dfCommonGenes.ind)
 
-m = as.matrix(dfCommonGenes[,c(9, 10, 11)])
-head(m)
-i = which(rowSums(m) >= 2)
-length(i)
-
-dfCommonGenes.ni = dfCommonGenes[i,]
-dim(dfCommonGenes.ni)
-
-table(dfCommonGenes.ni$Symbol %in% dfCommonGenes.ind$Symbol)
-
-## combine the p-values
 names(ldfData)
-cvTitle
-names(ldfData)[c(1,3,5)]
+names(ldfData)[c(1, 3, 5)]
+## extract the relevant information
 pv = sapply(ldfData[c(1, 3, 5)], function(x) return(x$adj.P.Val))
 rownames(pv) = ldfData[[1]]$ind
 pv = pv[as.character(dfCommonGenes.ind$ENTREZID), ]
+colnames(pv) = colnames(m)
 identical(rownames(pv), as.character(dfCommonGenes.ind$ENTREZID))
-fp = apply(pv, 1, f_fishersMethod)
-hist(fp)
-dfCommonGenes.ind$f.pvalue = fp
 
-## for non-induced group
-names(ldfData)
-cvTitle
-names(ldfData)[c(8, 9, 10)]
-pv = sapply(ldfData[c(8, 9, 10)], function(x) return(x$adj.P.Val))
-rownames(pv) = ldfData[[1]]$ind
-pv = pv[as.character(dfCommonGenes.ni$ENTREZID), ]
-identical(rownames(pv), as.character(dfCommonGenes.ni$ENTREZID))
-fp = apply(pv, 1, f_fishersMethod)
-hist(fp)
-dfCommonGenes.ni$f.pvalue = fp
+fc = sapply(ldfData[c(1, 3, 5)], function(x) return(x$logFC))
+rownames(fc) = ldfData[[1]]$ind
+fc = fc[as.character(dfCommonGenes.ind$ENTREZID), ]
+colnames(fc) = colnames(m)
+identical(rownames(fc), rownames(pv))
 
-write.csv(dfCommonGenes.ind, file='results/commonDEGenes_induced_2of3.xls')
-write.csv(dfCommonGenes.ni, file='results/commonDEGenes_notInduced_2of3.xls')
+### apply the filters
+f_findNearest2 = function(v){
+  d = as.matrix(dist(v))
+  diag(d) = NA
+  d = arrayInd(which.min(d), dim(d))
+  return(as.vector(d))
+}
+
+mN = t(apply(fc, 1, f_findNearest2))
+
+## take average of the nearest 2
+iAv = sapply(1:nrow(fc), function(x){
+  return(mean(fc[x, mN[x,]]))
+})
+
+## is the absolute difference between the 2 fc average
+## and the 3rd farthest fc greater than 1
+i = sapply(1:nrow(fc), function(x){
+  return(fc[x,-mN[x,]])
+})
+iAvd = abs(iAv - i)
+fDrop = iAvd > 1
+table(fDrop)
+
+iAv3 = rowMeans(fc)
+### replace the specific averages that are from 2 fc
+iAv3[fDrop] = iAv[fDrop]
+
+## calculate fisher p-values
+fp3 = apply(pv, 1, f_fishersMethod)
+fp2 = sapply(1:nrow(pv), function(x){
+  return(f_fishersMethod(pv[x, mN[x,]]))
+})
+
+fpC = fp3
+fpC[fDrop] = fp2[fDrop]
+
+identical(as.character(dfCommonGenes.ind$ENTREZID), names(iAv3))
+identical(as.character(dfCommonGenes.ind$ENTREZID), names(fpC))
+
+dfCommonGenes.ind$logFC = iAv3
+dfCommonGenes.ind$f.pvalue = fpC
+dfCommonGenes.ind$Outliers = fDrop
+data.frame(colnames(dfCommonGenes.ind))
+colnames(fc)
+temp = dfCommonGenes.ind[,c(1, 2, 4, 6, 14:17)]
+
+write.csv(temp, file='results/commonDEGenes_induced_2of3_logFC_average.xls')
